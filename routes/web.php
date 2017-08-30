9a091f4f8adf10c9cfc727dfb9d25da487510104
janjanarnaldo <janjanarnaldo@gmail.com>
2017-08-30 09:05:46 +0800

    added new apis

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,30 @@ $app->get('/', function () use ($app) {
     return $app->version();
 });
 
-$app->get('/ContactsGet', 'ContactController@showAll');
-$app->get('/ContactsGet/{id}', 'ContactController@show');
-
+// Contacts
 $app->post('/ContactsSet', 'ContactController@store');
 $app->post('/ContactsSet/{id}', 'ContactController@update');
-
+$app->get('/ContactsGet', 'ContactController@showAll');
+$app->get('/ContactsGet/{id}', 'ContactController@show');
 $app->delete('/ContactsDelete/{id}', 'ContactController@destroy');
+
+
+// Candidates
+$app->post('/CandidateSet', 'CandidateController@store');
+$app->post('/CandidateSet/{id}', 'CandidateController@update');
+$app->get('/CandidateGet', 'CandidateController@showAll');
+$app->get('/CandidateGet/{id}', 'CandidateController@show');
+$app->delete('/CandidateDelete/{id}', 'CandidateController@destroy');
+
+// Voters
+$app->post('/VoterSet', 'VoterController@store');
+$app->post('/VoterSet/{id}', 'VoterController@update');
+$app->get('/VoterGet', 'VoterController@showAll');
+$app->get('/VoterGet/{id}', 'VoterController@show');
+$app->post('/VoterLogin/{id}', 'VoterController@voterLogin');
+
+// Votes
+$app->post('/VoteSet', 'VoteController@store');
+$app->post('/VoteSet/{id}', 'VoteController@update');
+$app->get('/VoteGet', 'VoteController@showAll');
+$app->get('/VoteGet/{id}', 'VoteController@show');
